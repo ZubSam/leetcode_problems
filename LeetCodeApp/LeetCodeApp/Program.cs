bf2a481220575e7ce0ed4e00ef114f3acf9160f1
Samat Yeshenkulov <yeshenk.s@gmail.com>
2020-10-15 13:17:32 +0600

    commit

--- LeetCodeApp/LeetCodeApp/Program.cs
+++ LeetCodeApp/LeetCodeApp/Program.cs
@@ -9,60 +9,15 @@ namespace LeetCodeApp
     {
         static void Main(string[] args)
         {
-            var groupSizes = new int[] { 3, 3, 3, 3, 3, 1, 3 };
+            
 
-            var dict = new Dictionary<int, List<int>>();
-            var list = new List<int[]>();
-            for (int i = 0; i < groupSizes.Length; i++)
-            {
-                if (dict.ContainsKey(groupSizes[i]))
-                {
-                    dict[groupSizes[i]].Add(i);
-                }
-                else
-                {
-                    dict.Add(groupSizes[i], new List<int>() { i });
-                }
-            }
+            var nums1 = new int[] { 2 };
 
-            for (int i = 0; i < dict.Count; i++)
-            {
-                var item = dict.ElementAt(i);
+            var nums2 = new int[] {  };
 
-                var lenght = item.Value.Count;
+            
+            
 
-                if(item.Key == lenght)
-                {
-                    var arr1 = new int[lenght];
-
-                    for (int j = 0; j < lenght; j++)
-                    {
-                        arr1[j] = item.Value[j];
-                    }
-                    list.Add(arr1);
-                }
-                else
-                {
-                    var r = lenght / item.Key;
-                    var count = 0;
-
-                    for (int j = 0; j < r; j++)
-                    {
-                        var arr1 = new int[item.Key];
-
-                        for (int k = 0; k < item.Key; k++)
-                        {
-                            arr1[k] = item.Value[k + count];
-                        }
-
-                        count += item.Key;
-
-                        list.Add(arr1);
-                    }
-                }
-
-                
-            }
 
             Console.ReadKey();
         }
--- LeetCodeApp/LeetCodeApp/SolvedProblems/Hard.cs
+++ LeetCodeApp/LeetCodeApp/SolvedProblems/Hard.cs
@@ -4,7 +4,75 @@ using System.Text;
 
 namespace LeetCodeApp.SolvedProblems
 {
-    class Hard
+    public class Hard
     {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            double med = 0;
+
+            var l1 = nums1.Length;
+
+            var l2 = nums2.Length;
+
+            var lS = l1 + l2;
+
+            if (l1 > 0 && l2 > 0)
+            {
+                var nums3 = new int[lS];
+
+                Array.Copy(nums1, nums3, l1);
+                Array.Copy(nums2, 0, nums3, l1, l2);
+
+                Array.Sort(nums3);
+
+
+
+                if (lS % 2 == 0)
+                {
+                    var a1 = nums3[lS / 2 - 1];
+                    var a2 = nums3[lS / 2];
+
+                    med = (a1 + a2) / 2.0;
+                }
+                else
+                {
+                    med = nums3[(lS - 1) / 2];
+                }
+            }
+            else if (l1 > 0 && l2 == 0)
+            {
+                Array.Sort(nums1);
+
+                if (l1 % 2 == 0)
+                {
+                    var a1 = nums1[l1 / 2 - 1];
+                    var a2 = nums1[l1 / 2];
+
+                    med = (a1 + a2) / 2.0;
+                }
+                else
+                {
+                    med = nums1[(l1 - 1) / 2];
+                }
+            }
+            else if (l1 == 0 && l2 > 0)
+            {
+                Array.Sort(nums2);
+
+                if (l2 % 2 == 0)
+                {
+                    var a1 = nums2[l2 / 2 - 1];
+                    var a2 = nums2[l2 / 2];
+
+                    med = (a1 + a2) / 2.0;
+                }
+                else
+                {
+                    med = nums2[(l2 - 1) / 2];
+                }
+            }
+
+            return med;
+        }
     }
 }
